6ab32a8cfb6803b650e99261dcddc42e5bc4b0e9
Eirik <eirik@erik.com>
2021-02-14 21:54:36 +0100

    Factorisation des utilisateurs, mots de passe et IP

--- souriciere.sh
+++ souriciere.sh
@@ -7,12 +7,13 @@ rm ip.txt ip mdp1.*
 DATE_DU_JOUR=$(date +"%y%m%d")
 FICHIER_DU_JOUR=passwords.${DATE_DU_JOUR}
 
-#awk '/^host/ {sub("[[:cntrl:]]","");print $3":"$7":"$11}' passwords > ${FICHIER_DU_JOUR}
-
+premiere_extraction() {
 #Retire les caractères de contrôle.
 awk '/^host/ {sub("[[:cntrl:]]","");print $11}' passwords | sed '/^$/d' > mdp.${DATE_DU_JOUR}
 awk '/^host/ {sub("[[:cntrl:]]","");print $7}' passwords > util.${DATE_DU_JOUR}
 awk '/^host/ {sub("[[:cntrl:]]","");print $3}' passwords > ip.${DATE_DU_JOUR}
+}
+
 
 utilisateur() {
 #crée un fichier avec grep -Fxc -- et un sans. puis faire un diff pour voir s'il y a des différences.
@@ -31,9 +32,8 @@ while read ligne; do
 done < mdp_uniq.${DATE_DU_JOUR}
 }
 
-utilisateur
-mot_de_passe
 
+ip_localisation() {
 #Mettre la variable LOCALISATION entre guillemet sinon les lieux composé sont découpés sur plusieurs lignes
 #Un problème avec la république de Corée.
 while read ligne; do
@@ -48,6 +48,16 @@ cut -d':'  -f2 ip_localisation.${DATE_DU_JOUR} | sort -u >> ip_localisation_uniq
 while read ligne; do
 	printf "%s %s\n" "${ligne}" $(grep -Fc -- "${ligne}" ip_localisation.${DATE_DU_JOUR}) >> ip_fin.${DATE_DU_JOUR}
 done < ip_localisation_uniq.${DATE_DU_JOUR}
+}
+
+premiere_extraction
+
+utilisateur
+
+mot_de_passe
+
+ip_localisation
+
 
 sort -nk2 util_fin.${DATE_DU_JOUR} | tail -10 > util_fin_tri.${DATE_DU_JOUR}
 sort -nk2 mdp_fin.${DATE_DU_JOUR} | tail -10 > mdp_fin_tri.${DATE_DU_JOUR}
